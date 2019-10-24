b6bab3d45963d267034340c7b1972e0f0bafdbd4
suong4554 <43896957+suong4554@users.noreply.github.com>
2019-10-24 18:07:31 +0800

    random forest

--- ML Group Project/scripts/main.py
+++ ML Group Project/scripts/main.py
@@ -6,7 +6,7 @@ import os
 import linearAlgo
 import logAlgo
 import kncAlgo
-
+import ranForestClassifierAlgo as rfCA
 
 # returns the data from the Exel table:
 def load_df(dir_path, file_name):
@@ -58,5 +58,32 @@ for k in k_range:
 max_accuracy = max(scores)
 max_index = scores.index(max_accuracy) + 1 # function starts to count from 0
 print("\t Maximal accuacy (" + str(max_accuracy) + "%) with k = " + str(max_index))
+
+# apply Random Forest Classifier Algorithm:
+max_depth = 9
+estimators = 43
+y_prediction = rfCA.apply_random_forest_classifier(train_x, train_y, test_x, max_depth, estimators)
+
+display_accuracy(y_prediction, test_y, "Random Forest Classifier (max_depth = " + str(max_depth) + ", estimators = " + str(estimators) + ")")
+
+#Test different estimator values
+scores = []
+estimators_range = list(range(1, 30))
+max_depth_range = list(range(1, 5))
+
+for estimators in estimators_range:
+    for max_depth in max_depth_range:
+        temp = []
+        actual_prediction = rfCA.apply_random_forest_classifier(train_x, train_y, test_x, max_depth, estimators)
+        temp.append(accuracy_score(test_y, actual_prediction))
+        temp.append(estimators)
+        temp.append(max_depth)
+        scores.append(temp)
+
+scores0 = [_[0] for _ in scores]
+
+max_accuracy = max(scores0)
+max_index = scores0.index(max_accuracy) + 1 # function starts to count from 0
+print("\t Maximum accuacy (" + str(max_accuracy) + "%) with estimator = " + str(scores[max_index][1]) + " with max_depth = " + str(scores[max_index][2]))
 
 # apply
