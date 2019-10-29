6bd1c052f01a8215afdc18e5a0540ec66a819c61
suong4554 <43896957+suong4554@users.noreply.github.com>
2019-10-29 19:32:22 +0800

    Update tuning.py

--- ML_Group_Project/scripts/tuning.py
+++ ML_Group_Project/scripts/tuning.py
@@ -30,14 +30,14 @@ def apply_logistic_regression(train_x, train_y, test_x):
         fit_intercept=True,
         intercept_scaling=1, 
         max_iter=100, 
-        multi_class='warn',
+        multi_class='auto',
         n_jobs=None, 
         penalty='l1',
         random_state=None, 
-        #solver='lbfgs',
+        solver='liblinear',
         tol=0.0001, 
-        verbose=0, 
-        warm_start=False)
+        verbose=5, 
+        warm_start=True)
         
     lr.fit(train_x, train_y)
 
